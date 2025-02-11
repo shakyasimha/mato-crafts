65f4ff774a56cd3949253da93bc22ef8111f4500
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2025-02-11 11:11:32 +0545

    class attributes defined for Product class; will start getting added

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,26 @@
 # from django.db import models
-from backend.queryset import db 
-
+from dataclasses import dataclass, field
+from typing import List, Dict
+from bson import ObjectID
 # Create your models here.
-
+@dataclass
+class Product:
+    """Class for products document"""
+    
+    # Class attributes start here
+    _id: ObjectID = field(default_factory=ObjectID)
+    name: str = None
+    images: List[str] = field(default_factory=list)
+    price: int = 0
+    stock_remaining: int = 0
+    recommender_metadata: Dict[str, any] = field(default_factory=dict)
+    size: List[str] = field(default_factory=list)
+    description: str = None
+    materials: List[str] = field(default_factory=list)
+    weight: int = 0
+    dimension: str = None
+    product_type: List[str] = field(default_factory=list)
+    categories: List[str] = field(default_factory=list)
+    reviews: ObjectID = field(default_factory=ObjectID)
+    
+    
