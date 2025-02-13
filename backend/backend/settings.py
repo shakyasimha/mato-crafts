428d5e84a17e6b169321d20a23d885781679e225
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2025-02-13 16:23:39 +0545

    reediting all the code; setting up postgres:

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,12 +75,16 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mato-crafts",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5433",
+    }
+}
 
 # Rest Framework settings here
 REST_FRAMEWORK = {
