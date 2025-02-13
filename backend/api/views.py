f171cf52a68a587d839242f718b0aed6586f8215
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2025-02-13 16:30:18 +0545

    first migration to postgres

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,11 +3,6 @@ from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from rest_framework import status 
 
-from .queryset import QuerySet 
-from .models import (
-    
-)
-
 # Create your views here.
 @api_view(['GET', 'POST'])
 def test_api(request):
