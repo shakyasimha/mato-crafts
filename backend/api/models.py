b8c46507498820cec846d2cfc2b0870fec4f8e91
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2025-02-14 13:58:15 +0545

    added junction tables

--- backend/api/models.py
+++ backend/api/models.py
@@ -34,13 +34,6 @@ class Employee(models.Model):
             ('S', 'Salesperson')
         ]
     )
-
-class Review(models.Model):
-    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer')
-    description = models.TextField() 
-    rating = models.IntegerField(max_length=1, choices=[0, 1, 2, 3, 4, 5])
-    images = ArrayField(models.URLField(max_length=255), blank=True, default=list) 
-
 class Product(models.Model):
     name = models.CharField(max_length=255, null=False)
     price = models.FloatField()
@@ -60,21 +53,47 @@ class Product(models.Model):
     weight = models.IntegerField()
     dimension = models.CharField(max_length=255)
     type_of = models.CharField(max_length=16, choices=['Pair', 'Single', 'Bundle'])
-    reviews = models.ForeignKey(Review, related_name='products')
+    # reviews = models.ForeignKey(Review, on_delete=models.SET_NULL, related_name='products')
     categories = models.CharField(max_length=255)
     
 class ProductListing(models.Model):
-    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    # product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
     quantity = models.IntegerField() 
     listing_price = models.IntegerField() 
     coupon_discount = models.IntegerField()
-    
+
+class Review(models.Model):
+    # customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='customer')
+    description = models.TextField() 
+    rating = models.IntegerField(max_length=1, choices=[0, 1, 2, 3, 4, 5])
+    images = ArrayField(models.URLField(max_length=255), blank=True, default=list)
+
 class Sales(models.Model):
     buyer_name = models.CharField(max_length=255, blank=False)
     price = models.IntegerField()
     verified = models.BooleanField()
 
 class Cart(models.Model):
-    product_listing_id = models.ForeignKey(ProductListing, on_delete=models.CASCADE, related_name='product-list')
-    buyer_id = models.ForeignKey(Customer)
     paid = models.CharField(max_length=16, choices=['Paid', 'Failed', 'Pending'])
+
+
+## Junction tables start here
+class ProductReviewJunction(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    review_id = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='review')
+    
+class ProductListingJunction(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    product_listing_id = models.ForeignKey(ProductListing, on_delete=models.CASCADE, related_name='product-listing')
+
+
+class CartJunction(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    buyer_id = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='buyer')
+    product_listing_id = models.ForeignKey(ProductListing, on_delete=models.CASCADE, related_name='products')
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart')
+
+class SalesCartJunction(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='products')
+    sales_id = models.ForeignKey(Sales, on_delete=models.CASCADE, related_name='sales')
+    cart_id = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='cart')
